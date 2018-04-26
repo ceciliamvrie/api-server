f580a5d3530c77057a2ab666d8269192f73aa57d
Rodolfo Rodriguez <rodolfobgibson@gmail.com>
2018-04-26 10:31:11 -0500

    test artistStorage.FromArtist

--- pkg/postgres/artist_test.go
+++ pkg/postgres/artist_test.go
@@ -3,6 +3,7 @@ package postgres_test
 import (
 	"os"
 	"testing"
+	"time"
 
 	_ "github.com/golang-migrate/migrate/source/file"
 	_ "github.com/lib/pq"
@@ -45,6 +46,22 @@ func TestLoad(t *testing.T) {
 	}
 }
 
+func TestFromFestival(t *testing.T) {
+	ff, _, migrateDown := storeFestivals(t)
+	defer migrateDown()
+
+	aStore := postgres.NewArtistStorage(os.Getenv("PG_TEST_DSN"))
+
+	aclAa, err := aStore.FromFestival("Austin City Limits")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !lineupEqual(ff[0].Lineup, aclAa) {
+		t.Fatalf("error retriving artists from 'Austin City Limits': have %v, want %v", aclAa, ff[0].Lineup)
+	}
+}
+
 func storeArtists(t *testing.T) (aa []lineuplist.Artist, as lineuplist.ArtistStorage, migrateDown func()) {
 	aa = []lineuplist.Artist{
 		{Name: "Kanye West"},
@@ -63,4 +80,55 @@ func storeArtists(t *testing.T) (aa []lineuplist.Artist, as lineuplist.ArtistSto
 	}
 
 	return aa, aStore, func() { postgres.MigrateDown(os.Getenv("PG_TEST_DSN")) }
+}
+
+func storeFestivals(t *testing.T) (ff []lineuplist.Festival, fs lineuplist.FestivalStorage, migrateDown func()) {
+	ff = []lineuplist.Festival{
+		{
+			Name:      "Austin City Limits",
+			StartDate: time.Now(), EndDate: time.Now(),
+			Country: "United States", State: "Tx", City: "Austin",
+			Lineup: []lineuplist.Artist{{Name: "Red Hot Chilli Peppers"}, {Name: "Gorillaz"}, {Name: "Jay-Z"}},
+		},
+		{
+			Name:      "Levitation",
+			StartDate: time.Now(), EndDate: time.Now(),
+			Country: "United States", State: "Tx", City: "Austin",
+			Lineup: []lineuplist.Artist{{Name: "Gorillaz"}, {Name: "The Octopus Project"}, {Name: "Ariel Pink"}},
+		},
+	}
+
+	postgres.MigrateUp(os.Getenv("PG_TEST_DSN"))
+
+	fStore := postgres.NewFestivalStorage(os.Getenv("PG_TEST_DSN"))
+
+	for _, f := range ff {
+		_, err := fStore.Save(f)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	return ff, fStore, func() { postgres.MigrateDown(os.Getenv("PG_TEST_DSN")) }
+}
+
+func lineupEqual(a, b []lineuplist.Artist) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := 0; i < len(a); i++ {
+		found := false
+		for j := 0; j < len(a); j++ {
+			if a[i].Name == b[i].Name {
+				found = true
+				continue
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+
+	return true
 }
